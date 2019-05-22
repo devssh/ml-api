0f2a4ee55217586a482744982edcc45dc1a82d59
Devashish Sood <devasood@gmail.com>
2019-05-22 17:54:17 +0530

    Add tests

---
+++ clients/test.sh
@@ -0,0 +1,6 @@
+curl -XPOST 'localhost:5001/face/get_bounding_rects' -F "pimage=@/Users/devashishsood/Downloads/faces.png" -F "auth=c9095970345d"
+
+curl -XPOST 'localhost:5001/gender/extract_from_image' -F "pimage=@/Users/devashishsood/Documents/devashish.png" -F "auth=c9095970345d"
+
+curl -XPOST 'localhost:5001/gender/extract_from_name' -d '{"names": "devashish,gayathri", "auth": "c9095970345d"}'
+
